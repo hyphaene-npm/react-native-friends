0ec949a16931a5880bcafb6938ec9177ed9b36c5
Maximilien Garenne <maximilien@whatson-web.com>
2018-10-17 16:10:44 +0200

    init reactifire@1.0.6

---
+++ __tests__/example.js
@@ -0,0 +1,7 @@
+import react-native-friends from '../src/';
+
+describe('react-native-friends', () => {
+	it('should return the proper value', () => {
+		expect(react-native-friends).toBeInstanceOf(Function);
+	});
+});
---
+++ example/src/App.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ExampleComponent from 'react-native-friends';
+import 'react-native-friends/build/index.css';
+
+const App = () => <ExampleComponent name="Maximilien Garenne" />;
+
+export default App;
